da746015591584cacbf58f087bc06637102a678a
yKicchan <r7@ykicchan.com>
2025-10-07 13:03:56 +0900

    test: src 以下にキャッシュ出るのが邪魔なので root 使わない

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -3,14 +3,14 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   plugins: [],
   test: {
-    root: "src",
     globals: true,
     env: {
       TZ: "Asia/Tokyo",
     },
     coverage: {
-      reportsDirectory: "../artifacts/coverage",
+      reportsDirectory: "./artifacts/coverage",
       reporter: ["text", "json-summary", "html"],
+      include: ["src/**/*.ts"],
     },
   },
 });
